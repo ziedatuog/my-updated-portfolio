c00dc9f87af0bc484db387fff42be9a64ae3dea3
zelalem <dezelalem31@gmail.com>
2024-12-05 05:44:40 -1200

    commit the public index

--- src/App.js
+++ src/App.js
@@ -1,45 +1,4 @@
   
-// import './App.scss';
-// import { Routes, Route, useLocation } from 'react-router-dom';
-// import Home from './containers/home';
-// import About from './containers/about';
-// import Contact from './containers/contact';
-// import Portfolio from './containers/portfolio';
-// import Skills from './containers/skills';
-// import Resume from './containers/resume'; 
-// import Navbar from './components/navBar';
- 
-// import ParticlesComponent from './utils/particles';
-
-// function App() {
-
-//   const location = useLocation()
-   
-//   return (
-//     <div className="App">
-//       {location.pathname === '/' && <ParticlesComponent id="particles"   />}
-//       <Navbar />
-
-//       {/* main page content */}
-//       <div className='App__main-page-content'>
-//       <Routes>
-//         <Route index path="/" element={<Home />} />
-//         <Route path="/about" element={<About />} />
-//         <Route path="/contact" element={<Contact />} />
-//         <Route path="/portfolio" element={<Portfolio />} />
-//         <Route path="/skills" element={<Skills />} />
-//         <Route path="/resume" element={<Resume />} />
-//       </Routes>
-//       </div>
-      
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
 import "./App.scss";
 import { Routes, Route, useLocation } from "react-router-dom";
 import Particles from "react-tsparticles";
