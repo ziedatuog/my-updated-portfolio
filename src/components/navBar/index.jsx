68e878e2047cf91173837e36cb95fd982fd8f33e
zelalem <dezelalem31@gmail.com>
2024-12-10 23:46:35 -1200

    Your commit message

--- src/components/navBar/index.jsx
+++ src/components/navBar/index.jsx
@@ -1,4 +1,60 @@
   
+// import React, { useState } from "react";
+// import { FaBars, FaReact } from "react-icons/fa";
+// import { HiX } from "react-icons/hi";
+// import { Link } from "react-router-dom";
+// import "./style.scss";
+
+// const data = [
+//   { label: "HOME", to: "/" },
+//   { label: "ABOUT ME", to: "/about" },
+//   { label: "SKILLS", to: "/skills" },
+//   { label: "RESUME", to: "/resume" },
+//   { label: "PORTFOLIO", to: "/portfolio" },
+//   { label: "CONTACT", to: "/contact" },
+// ];
+
+// const Navbar = () => {
+//   const [toggleIcon, setToggleIcon] = useState(false);
+
+//   const handleToggleIcon = () => {
+//     setToggleIcon(!toggleIcon);
+//   };
+
+//   return (
+//     <nav className="navbar">
+//       <div className="navbar__container">
+//         <Link to={"/"} className="navbar__container__logo">
+//           <FaReact size={30} />
+//         </Link>
+
+//         <ul className={`navbar__container__menu ${toggleIcon ? "active" : ""}`}>
+//           {data.map((item, key) => (
+//             <li key={key} className="navbar__container__menu__item">
+//               <Link
+//                 className="navbar__container__menu__item__links"
+//                 to={item.to}
+//               >
+//                 {item.label}
+//               </Link>
+//             </li>
+//           ))}
+//         </ul>
+
+//         <div className="nav-icon" onClick={handleToggleIcon}>
+//           {toggleIcon ? <HiX size={30} /> : <FaBars size={30} />}
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
+
+
+
+
+
 import React, { useState } from "react";
 import { FaBars, FaReact } from "react-icons/fa";
 import { HiX } from "react-icons/hi";
@@ -21,10 +77,14 @@ const Navbar = () => {
     setToggleIcon(!toggleIcon);
   };
 
+  const handleMenuClick = () => {
+    setToggleIcon(false); // Close the menu when an item is clicked
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar__container">
-        <Link to={"/"} className="navbar__container__logo">
+        <Link to={"/"} className="navbar__container__logo" onClick={handleMenuClick}>
           <FaReact size={30} />
         </Link>
 
@@ -34,6 +94,7 @@ const Navbar = () => {
               <Link
                 className="navbar__container__menu__item__links"
                 to={item.to}
+                onClick={handleMenuClick} // Close the menu on link click
               >
                 {item.label}
               </Link>
